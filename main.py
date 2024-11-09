9c932fa61132db6154dec987d82e13746d0b3a1b
PavloFurhalo <p.furgalo2006@gmail.com>
2024-11-09 10:58:11 +0200

    payment page

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ def home():
 @app.route('/good')
 def good():
     return render_template('good.html')
+@app.route('/paiment')
+def paiment():
+    return render_template('paiment.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
