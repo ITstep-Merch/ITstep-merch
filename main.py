70e739fe4c066484a9640e7a5a90f4c502082a27
Vitaliy <bering856@gmail.com>
2024-11-11 14:55:42 +0200

    add db

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User, Article
+from models import db, Good
 from datetime import datetime
 import os
 import dotenv
@@ -13,7 +13,7 @@ ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///merch.db"
 db.init_app(app)
 Bootstrap5(app)
 
@@ -22,13 +22,62 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    all_goods = db.session.execute(db.select(Good).order_by(Good.id.desc())).scalars().all()
+    return render_template('home.html', all_goods=all_goods)
+
 @app.route('/good')
 def good():
     return render_template('good.html')
-@app.route('/paiment')
-def paiment():
-    return render_template('paiment.html')
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
+
+@app.route('/admin/<password>', methods=["GET", "POST"])
+def admin(password):
+    if password == ADMIN_PASSWORD:
+        if request.method == "POST":
+            new_good = Good(
+                name=request.form.get('name'),
+                img_url=request.form.get('img_url'),
+                code=request.form.get('code'),
+                price=request.form.get('price'),
+                color=request.form.get('color'),
+                size=request.form.get('size'),
+                description=request.form.get('description'),
+                made_of=request.form.get('made_of'),
+            )
+            db.session.add(new_good)
+            db.session.commit()
+
+            render_template('admin.html', password=password)
+            
+        return render_template('admin.html', password=password)
+    else: 
+        return abort(403)
+    
+@app.route('/admin/home/<password>', methods=["GET", "POST"])
+def admin_home(password):
+    if password == ADMIN_PASSWORD:
+        if request.method == "POST":
+            new_good = Good(
+                name=request.form.get('name'),
+                img_url=request.form.get('img_url'),
+                code=request.form.get('code'),
+                price=request.form.get('price'),
+                color=request.form.get('color'),
+                size=request.form.get('size'),
+                description=request.form.get('description'),
+                made_of=request.form.get('made_of'),
+            )
+            db.session.add(new_good)
+            db.session.commit()
+
+            render_template('admin.html', password=password)
+            
+        return render_template('admin.html', password=password)
+    else: 
+        return abort(403)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
